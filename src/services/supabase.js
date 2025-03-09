408b84f97b6d402b4de4176509e5ce386c4f8303
M Satish Kumar <programmingwithsatish@gmail.com>
2025-03-09 17:28:29 +0530

    Supabase up and running with all data

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,10 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 // import { serve } from "https://deno.land/std@0.202.0/http/server.ts";
 // import { corsHeaders } from "../../../supabase/functions/_shared/cors";
 
-export const supabaseUrl = "https://lyhygstmyfkkurwpzwkx.supabase.co";
+export const supabaseUrl = "https://rhxfuzfkhbmhobfukfxn.supabase.co";
 
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5aHlnc3RteWZra3Vyd3B6d2t4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI1MjQ0MDUsImV4cCI6MjAzODEwMDQwNX0.YSfrKYcf7b6R8X9RspREHglbB7KbO6ZlsC7_dNowCyQ";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoeGZ1emZraGJtaG9iZnVrZnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE1MTE3NDksImV4cCI6MjA1NzA4Nzc0OX0.qPRfCmNaXbQxQpXz9QYFa8BLK8K5SnpF7AhO7SzDA78";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
